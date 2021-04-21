a5c57c322db38c52837bc5e969da10cda36c1c25
sharathkaarthick <63258213+sharathkaarthick@users.noreply.github.com>
2021-04-21 17:22:56 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -43,7 +43,7 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#contact">
-                FAQ's
+                Message us
               </a>
             </li>
           </ul>
